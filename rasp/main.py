c6fdc058036b2733bbe294d1da5336770f8738e5
Pedro Matumoto <pedromatumoto@gmail.com>
2024-08-28 18:29:42 -0300

    feat(api-rasp): adiciona funcionamento com display e com bomba de agua)

--- rasp/main.py
+++ rasp/main.py
@@ -1,83 +1,67 @@
 import RPi.GPIO as GPIO
-import adafruit_dht
+import Adafruit_DHT as dht
 import board
 import time
 import requests
-from pymongo.mongo_client import MongoClient
-from pydantic import BaseModel
-from motor.motor_asyncio import AsyncIOMotorClient
-import Adafruit_SSD1306
+# from pymongo.mongo_client import MongoClient
+# from pydantic import BaseModel
+# from motor.motor_asyncio import AsyncIOMotorClient
+from luma.core.interface.serial import i2c
+from luma.core.render import canvas
+from luma.oled.device import ssd1306
 from PIL import Image
 from PIL import ImageDraw
 
 # Define o tipo de sensor
 pino_bomba = 26
-endpoint = "https://tamakabin.vercel.app/api/data"
+endpoint = "http://tamakabin.vercel.app/api/data"
 # client = MongoClient(uri)
 # database = client["tomferrite"]
 # collection = database["enzosakamoto"]
+beatmap = [
+    ['❄️', '🥶', '🌞', '🔥'],
+    ['🌵', '☀️', '🌅', '🌞'],
+    ['🌙', '🌌', '🌜', '🌟']
+]
 
-dht_device = adafruit_dht.DHT22(board.D4)
-PIN = 3
+PIN = 4
 GPIO.setmode(GPIO.BCM)
+sensor = dht.DHT22
 GPIO.setup(PIN, GPIO.IN)
+GPIO.setup(pino_bomba, GPIO.OUT)
+DHT = 21
 
-# Configura o display OLED
-RST = (
-    24  # embora nao utilizado de fato, eh preciso defini-lo para a biblioteca funcionar
-)
 
-# Configura uso do display OLED de 128x64 (comunicacao I²C)
-disp = Adafruit_SSD1306.SSD1306_128_64(rst=RST)
-
-# Inicializa biblioteca de comunicacao com display e o limpa
-disp.begin()
-disp.clear()
-disp.display()
-
-# obtem altura e largura totais do display
-width = disp.width
-height = disp.height
+serial = i2c(port=1, address=0x3C)
+device = ssd1306(serial)
 
 # Preparacoes necessarias para apagar tela
 icone_frio = (
-    Image.open("assets/icone_frio")
-    .resize((disp.width, disp.height), Image.ANTIALIAS)
-    .convert("1")
+    Image.open("assets/icone_frio.png").convert("1")
 )
+icone_frio = icone_frio.resize(device.size)
 icone_quente = (
-    Image.open("assets/icone_quente")
-    .resize((disp.width, disp.height), Image.ANTIALIAS)
-    .convert("1")
+    Image.open("assets/icone_quente.png").convert("1")
 )
+icone_quente = icone_quente.resize(device.size)
 icone_seco = (
-    Image.open("assets/icone_seco")
-    .resize((disp.width, disp.height), Image.ANTIALIAS)
-    .convert("1")
+    Image.open("assets/icone_seco.png").convert("1")
 )
+icone_seco = icone_seco.resize(device.size)
 icone_sol = (
-    Image.open("assets/icone_sol")
-    .resize((disp.width, disp.height), Image.ANTIALIAS)
-    .convert("1")
+    Image.open("assets/icone_sol.png").convert("1")
 )
+icone_sol = icone_sol.resize(device.size)
 icone_nuvem = (
-    Image.open("assets/icone_nuvem")
-    .resize((disp.width, disp.height), Image.ANTIALIAS)
-    .convert("1")
+    Image.open("assets/icone_nuvem.png").convert("1")
 )
-
-image = Image.new("1", (width, height))  # imagem binaria (somente 1's e 0's)
-draw = ImageDraw.Draw(image)
-
+icone_nuvem = icone_nuvem.resize(device.size)
 
 try:
     while True:
         try:
-            temp = dht_device.temperature
-            umid = dht_device.humidity
-            print("ta aqui")
+            temp, umid = dht.read_retry(sensor,DHT)
             bright = GPIO.input(PIN)
-
             if umid is not None and temp is not None:
                 # postar as informações no endpoint
                 # try:
@@ -86,9 +70,9 @@ try:
                 # except Exception as e:
                 #    print(e)
                 try:
-                    print(temp, umid)
+                    print(temp, umid, bright)
                     #
-                    requests.post(
+                    response = requests.post(
                         endpoint,
                         json={
                             "temperature": temp,
@@ -96,59 +80,63 @@ try:
                             "brightness": bright,
                         },
                     )
+                    print(response.text)
                 except Exception as e:
                     print(e)
                     print("Erro ao postar dados")
                 time.sleep(5)
 
                 if umid < 40:
+                    print("Muito seco")
                     GPIO.output(pino_bomba, GPIO.HIGH)
                     time.sleep(2)
                     GPIO.output(pino_bomba, GPIO.LOW)
-                    disp.clear()
-                    disp.display()
-                    disp.image(icone_seco)
+                    with canvas(device) as draw:
+                        draw.text((0, 0), f"Muito Seco!\nRegando...", fill=255)
 
                 elif temp < 20:
-                    disp.clear()
-                    disp.display()
-                    disp.image(icone_frio)
+                    print("frio")
+                    device.display(icone_frio)
+                    with canvas(device) as draw:
+                        draw.text((0, 0), f"Muito frio!\nMe coloque pra dentro!", fill=255)
 
                 elif temp > 30:
-                    disp.clear()
-                    disp.display()
-                    disp.image(icone_quente)
+                    print("calor")
+                    with canvas(device) as draw:
+                        draw.text((0, 0), f"Muito Calor!\nMe coloque tire do sol!", fill=255)
 
                 elif (
                     bright == 1
                     and time.localtime().tm_hour > 6
                     and time.localtime().tm_hour < 18
                 ):
-                    disp.clear()
-                    disp.display()
-                    disp.image(icone_sol)
+                    print("luz de dia")
+                    with canvas(device) as draw:
+                        draw.text((0, 0), f"Sol! Ainda bem!", fill=255)
 
                 elif (
                     bright == 0
                     or time.localtime().tm_hour < 6
                     or time.localtime().tm_hour > 18
                 ):
-                    disp.clear()
-                    disp.display()
-                    disp.image(icone_nuvem)
+                    print("eita ta de noite")
+                    with canvas(device) as draw:
+                       draw.text((0, 0), f"Noite! Vou dormir...", fill=255)
 
                 else:
+                    print("tudo ok")
                     disp.clear()
-                    disp.display()
-                    draw.text((0, 0), f"Tudo ok", fill=255)
-                    draw.text((0, 0), f"Temperatura: {temp}°C", fill=255)
-                    draw.text((0, 10), f"Umidade: {umid}%", fill=255)
-                    draw.text((0, 20), f"Luminosidade: {bright}", fill=255)
+                    with canvas(device) as draw:
+                        draw.text((0, 0), f"Tudo ok", fill=255)
+                        draw.text((0, 0), f"Temperatura: {temp}°C", fill=255)
+                        draw.text((0, 10), f"Umidade: {umid}%", fill=255)
+                        draw.text((0, 20), f"Luminosidade: {bright}", fill=255)
 
             elif umid is None or temp is None:
                 print("Erro ao ler sensor")
                 time.sleep(5)
-        except:
-            print("Erro ao ler sensor")
+        except Exception as e:
+            print("Erro na parte sensor")
+            print(e)
 except:
     GPIO.cleanup()
